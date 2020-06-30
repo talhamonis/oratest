f0ce74b9c16c4f08d8aa435010099364b43bce8c
talhamonis <64822677+talhamonis@users.noreply.github.com>
2020-06-30 09:07:48 -0400

    Update COUNTRIES.sql

--- RedGate/Tables/COUNTRIES.sql
+++ RedGate/Tables/COUNTRIES.sql
@@ -2,6 +2,7 @@ CREATE TABLE "RedGate".countries (
   country_id CHAR(2 BYTE) NOT NULL,
   country_name VARCHAR2(40 BYTE) NOT NULL,
   region_id NUMBER,
+  region_name CHAR(2 BYTE),
   PRIMARY KEY (country_id),
   CONSTRAINT fk_countries_regions FOREIGN KEY (region_id) REFERENCES "RedGate".regions (region_id) ON DELETE CASCADE
 );
