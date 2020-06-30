a2a40f9d417bd3d67f170757c12720ff3becbb33
talha <talha@>
2020-06-30 09:15:08 -0400

    Added column

--- RedGate/Tables/COUNTRIES.sql
+++ RedGate/Tables/COUNTRIES.sql
@@ -3,6 +3,7 @@ CREATE TABLE "RedGate".countries (
   country_name VARCHAR2(40 BYTE) NOT NULL,
   region_id NUMBER,
   region_name CHAR(2 BYTE),
+  region_code NUMBER,
   PRIMARY KEY (country_id),
   CONSTRAINT fk_countries_regions FOREIGN KEY (region_id) REFERENCES "RedGate".regions (region_id) ON DELETE CASCADE
 );
